67c2e0b6996cbdfb70ca055c46d9a43a85930861
Dev-magno <magnolopes1212@gmail.com>
2025-05-26 16:22:13 -0300

    lógica if-else adicionada

--- batalha_de_cartas.c
+++ batalha_de_cartas.c
@@ -16,6 +16,9 @@
  * 
  * ------------SEGUNDA ATUALIÇÃO DO PROGRAMA------------
  * O programa foi atualizado para implementar a lógica de comparação entre as duas cartas do Super Trunfo (batalha de cartas), determinando a vencedora de cada atributo e introduzindo o conceito de "Super Poder". 
+ 
+ * ------------TERCEIRA ATUALIÇÃO DO PROGRAMA------------
+ * O foco deste nível é a comparação entre duas cartas já cadastradas, utilizando estruturas de decisão if e if-else para determinar, com base em um dos atributos escolhidos, a carta vencedora. 
  */
 #include <stdio.h>
 // Fornece a função strcspn para manipulação de strings, usada para remover '\n' do fgets
@@ -37,7 +40,7 @@ int main() {
 
     // Declaração das variáveis para armazenar os atributos da segunda carta
     char estado2[2]; 
-    char codigo_da_carta2[2]; 
+    char codigo_da_carta2[4]; 
     char nome_da_cidade2[100];
     unsigned long int populacao2; 
     float area2;
@@ -143,6 +146,7 @@ int main() {
     // Calcula o superpoder da carta
     superPoder2 = ((float)populacao2) + (area2) + (PIB2 * 1e9f) + ((float)numero_de_pontos_turisticos2) + (inverso_densidade2) + (PIB_per_capita2);
 
+    /*
     // ====== Comparação de cartas ======
     printf("\n=========================================\n");
     printf("\nCOMPARAÇÃO DE CARTAS:\n");
@@ -154,7 +158,7 @@ int main() {
     printf("PIB per Capita: Carta 1 venceu? %d\n", (PIB_per_capita1 > PIB_per_capita2));
     printf("Super Poder: Carta 1 venceu? %d\n", (superPoder1 > superPoder2));
     printf("\n=========================================\n");
-
+    */
 
 
     // ====== Exibir cartas ======
@@ -176,8 +180,6 @@ int main() {
     printf("\n=========================================\n");
 
 
-
-    printf("\n=========================================\n");
     printf("\n\nDADOS DA CARTA 2:\n");
     printf("Estado: %s\n", estado2);
     printf("Código: %s\n", codigo_da_carta2);
@@ -192,6 +194,64 @@ int main() {
     printf("PIB per capita: %.0f reais\n", PIB_per_capita2);
     printf("Superpoder: %.2f\n", superPoder2);
     printf("\n=========================================\n");
+
+
+
+      // Comparar atributos utilizando if-else para determinar a carta vencedora
+    printf("\nComparando atributos das cartas:\n");
+    printf("Atenção: O critério para determinar a carta vencedora é PIB per Capita\n");
+
+    if(populacao1 > populacao2) {
+        printf("Carta 1 venceu\n");
+    } else {
+        printf("Carta 2 venceu\n");
+    }
+
+    if(area1 > area2) {
+        printf("Carta 1 venceu\n");
+    } else {
+        printf("Carta 2 venceu\n");
+    }
+
+    if (PIB1 > PIB2) {
+        printf("Carta 1 venceu\n");
+    } else {
+        printf("Carta 2 venceu\n");
+    }
+
+    if(numero_de_pontos_turisticos1 > numero_de_pontos_turisticos2) {
+        printf("Carta 1 venceu\n");
+    } else {
+        printf("Carta 2 venceu\n");
+    }
+
+    if(inverso_densidade1 < inverso_densidade2) {
+        printf("Carta 1 Venceu\n");
+    } else {
+        printf("Carta 2 venceu\n");
+    }
+
+    if(PIB_per_capita1 > PIB_per_capita2) {
+        printf("Carta 1 venceu\n");
+    } else {
+        printf("Carta 2 venceu\n");
+    }
+
+    if(superPoder1 > superPoder2) {
+        printf("Carta 1 venceu\n");
+    } else {
+        printf("Carta 2 venceu\n");
+    }
+
+    printf("\n=========================================\n");
+    printf("Carta Vencedora (Atributo PIB per Capita):\n");
+    printf("Carta 1: %s: %.2f\n", nome_da_cidade1, PIB_per_capita1);
+    printf("Carta 2: %s: %.2f\n", nome_da_cidade2, PIB_per_capita2);
+    if(PIB_per_capita1 > PIB_per_capita2) {
+        printf("Resultado: Carta 1 (%s) venceu",nome_da_cidade1);
+    } else {
+        printf("Resultado: Carta 2 (%s) venceu", nome_da_cidade2);
+    }
 
     return 0;
 }
